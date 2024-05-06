091b5a4f7641a6113c3de3ceb14c3e12b2cd8152
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-05-05 18:21:04 -0700

    change .gitignore to attempt build fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UGYZbdd+7KAh4M1QDcdtcJHvhbhJvuD+idaaFuj7Ihw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tlPIhsbWPNShykL9s3yDH48ePzx1HaYjR88KB6ihnuQ=\"\n}"
