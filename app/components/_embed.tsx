7de7371fba912e1fb05661fb80103dc4994f96cd
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-05-10 21:39:21 -0700

    I don't care. Tailwind is fucking awful. I'll never use it again on a project.

--- app/components/_embed.tsx
+++ app/components/_embed.tsx
@@ -2,11 +2,12 @@
 export default function Embed() {
 	return (
 		<>
+		<div className="w-full">
+
 			<h2 className="text-4xl text-center font-bold">The Podcast</h2>
 			{/*width="560"*/}
-			<div className="w-screen p-5">
 				<iframe 
-					className="aspect-video"
+					className="aspect-video mt-10"
 					loading="lazy"
 					src="https://www.youtube.com/embed/videoseries?si=tR95OW0PLANq-WPn&amp;list=PLKcCKSGU8sp1FDtNcrge8BOCxVdgdGrsW"
 					title="Geeky Gamer Podcast" 
@@ -15,7 +16,7 @@ export default function Embed() {
 					referrerPolicy="strict-origin-when-cross-origin" 
 					allowFullScreen>
 				</iframe>
-			</div>
+		</div>
 		</>
 	);
 }
