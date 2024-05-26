c2b046fc7663934bac07db6731f398644e7aa231
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-05-26 00:29:13 -0700

    try adding guest list

--- app/components/_guest-list.tsx
+++ app/components/_guest-list.tsx
@@ -3,8 +3,9 @@ export default function GuestList() {
 	return (
 		<>
 			<div className="text-3xl text-center font-bold md:text-5xl">
-				GUEST LIST HERE
-			</div>
-		</>
-	);
+				<ul>
+				</ul>
+		</div>
+	</>
+);
 }
